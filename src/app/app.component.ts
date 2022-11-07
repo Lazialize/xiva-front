e2062dada038b5e72416dd8631e419065084e931
Lazinelia <lazinelia@gmail.com>
2022-11-07 09:12:20 +0900

    feat: implement localization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { loadMessages, locale } from 'devextreme/localization';
+import jaMessages from 'devextreme/localization/messages/ja.json';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    loadMessages(jaMessages);
+    locale(navigator.language);
+  }
 }
